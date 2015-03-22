09690e5585ec6a5ee533848b3c1ce6b13f8650f9
Anton Iškov <iskov.anton@yandex.com>
2015-03-22 09:51:29 +0100

    Update JiNGle.js

--- src/js/JiNGle.js
+++ src/js/JiNGle.js
@@ -1,12 +1,10 @@
-JiNGle = JiNGle || { };
+JiNGle = JiNGle || {};
 
 JiNGle.rootPath = null;
 JiNGle.scriptPath = null;
 JiNGle.viewPath = null;
 
-JiNGle.arguments = {};
-
-JiNGle.init = function() {
+(function() {
     var scripts = document.getElementsByTagName('script');
     var currentPath = scripts[scripts.length-1].src;
 
@@ -14,18 +12,4 @@ JiNGle.init = function() {
 
     this.scriptPath = this.rootPath + 'js/';
     this.viewPath = this.rootPath + 'views/';
-
-    var indexOfArg = currentPath.lastIndexOf('#?');
-    if (indexOfArg) {
-        currentPath.substring(indexOfArg + 2, currentPath.length - 1)
-            .split('&')
-            .forEach(function(part) {
-                var values =part.split('=');
-                JiNGle.arguments[decodeURIComponent(values[1])] = decodeURIComponent(values[0]);
-            });
-    }
-
-    if (this.arguments['automport'] == 'true') this.import(this.components);
-};
-
-JiNGle.init();
+})();
